82694a050d6ee18b9597940adc0a9d9a1b63890c
KAI-Miru <kamiru079@gmail.com>
2026-09-06 02:37:13 +0800

    miru: make UXIO sysctl storage type-safe

--- vendor/oplus/kernel/oplus_performance/oplus_uxio_first/oplus_uxio_first_opt.h
+++ vendor/oplus/kernel/oplus_performance/oplus_uxio_first/oplus_uxio_first_opt.h
@@ -13,7 +13,7 @@
 #define BLK_MIN_DEPTH_ON	16
 #define BLK_MAX_BG_DEPTH	24
 
-extern bool sysctl_uxio_io_opt;
+extern unsigned int sysctl_uxio_io_opt;
 extern bool blk_pm_allow_request(struct request *rq);
 extern struct request * smart_peek_request(struct request_queue *q);
 extern void queue_throtl_add_request(struct request_queue *q,
